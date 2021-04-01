f28d223926e99f0ea9ef13271fbc2cb73117c31d
meo <agv1291@gmail.com>
2021-04-01 17:46:00 +0900

    AddTaskleaf

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  resources :tasks
+
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  namespace :admin do
+   resources :users
+    
+  end
+
   root to:'tasks#index'
+  resources :tasks
 end
--- db/migrate/20210323195016_create_tasks.rb
+++ db/migrate/20210323195016_create_tasks.rb
@@ -8,3 +8,5 @@ class CreateTasks < ActiveRecord::Migration[5.2]
     end
   end
 end
+
+
